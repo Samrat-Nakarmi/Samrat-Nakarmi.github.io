db3e279fc6155e0179564fef63b869c38d5df48f
Samrat Nakarmi <samratnakarmi1@gmail.com>
2023-05-21 23:40:22 +0545

    final commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+const t2 = gsap.fromTo(".iam" , {y:0, opacity: 0},{y: 0, opacity:1,delay: 1.5 , duration: 1.5, ease:"power3"})
+const t3 = gsap.fromTo(".hi" , {y:110, opacity: 0},{y: 0, opacity: 1,delay: 2.2 , duration: 2, ease:"power3"})
+const t4 = gsap.fromTo(".hewwo" , {y:110, opacity: 0},{y: 0, opacity: 1,delay: 2.6 , duration: 2, ease:"power3"})
+const t5 = gsap.fromTo(".sammy", {opacity:0}, {opacity:1, delay: 3, duration: 2.8, ease:"power3"})
+const t6 = gsap.fromTo(".ssa", {x:110, opacity: 0}, {x: 0, opacity: 1, delay: 2.2 , duration: 2, ease:"power3"})
+const t7 = gsap.fromTo(".ssab", {x:110, opacity: 0}, {x: 0, opacity: 1, delay: 2.6 , duration: 2, ease:"power3"})
+
 const t9 = gsap.timeline({
     scrollTrigger:{
         trigger:'.sasfg',
@@ -6,14 +13,134 @@ const t9 = gsap.timeline({
         duration: 2,
         markers: true,
         }
-    }).fromTo('.sasfg', {y:"20%", opacity:0}, {y:0,opacity:1})
+}).fromTo('.sasfg', {y:"20%", opacity:0}, {y:0,opacity:1})
 
-const t1 = gsap.timeline({
+const t10 = gsap.timeline({
     scrollTrigger:{
-        trigger:'.qwe',
-        start:"-100px center",
+        trigger:'.sasfg',
+        start:"140px center",
         end:"bottom center",
         duration: 2,
         markers: true,
         }
-    }).fromTo('.sasfg', {y:"20%", opacity:0}, {y:0,opacity:1})
+}).fromTo('.sasfgh', {y:"20%", opacity:0}, {y:0,opacity:1})
+
+const t11 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sasfg',
+        start:"160px center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.sas', {y:"20%", opacity:0}, {y:0,opacity:1})
+
+const t12 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sasfg',
+        start:"500px center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.sussy', {y:"20%", opacity:0}, {y:0,opacity:1})
+
+const t13 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"100px center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.sussy1', {x: -110, opacity:0}, {x:0,opacity:1})
+
+const t14 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"200 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.sussy2', {x: 110, opacity:0}, {x:0,opacity:1})
+
+const t15 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"350 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.sussy3', {x: -110, opacity:0}, {x:0,opacity:1})
+
+const t16 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"450 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.sussy4', {x: 110, opacity:0}, {x:0,opacity:1})
+
+const t17 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"500 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.sussy5', {x: -110, opacity:0}, {x:0,opacity:1})
+
+const t18 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"600 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.qqq', {y: 110, opacity:0}, {y:0,opacity:1})
+
+const t19 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"700 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.qqq1', {y: 110, opacity:0}, {y:0,opacity:1})
+
+const t20 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"700 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.qqq1', {y: 110, opacity:0}, {y:0,opacity:1})
+
+const t21 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sussy',
+        start:"800 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.qqq1', {y: 110, opacity:0}, {y:0,opacity:1})
+
+const t22 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.sasfg',
+        start:"200 center",
+        end:"bottom center",
+        duration: 2,
+        markers: true,
+        }
+}).fromTo('.lool', {x: 110, opacity:0}, {x:0,opacity:1})
